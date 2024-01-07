891f61291b84dd2863c6b0c1063737fac99e7a71
Felix Peer <felixpeerpi@gmail.com>
2024-01-07 21:06:57 +0100

    moved barbell class to new barbell.dart

---
+++ barbell_calc/lib/barbell.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+class Barbell {
+  final int id;
+  final String name, width;
+  final double weight;
+
+  Barbell({
+    required this.id,
+    required this.name,
+    required this.width,
+    required this.weight,
+  });
+
+  factory Barbell.fromJson(Map<String, dynamic> jsonData) {
+    return Barbell(
+      id: jsonData['id'],
+      name: jsonData['name'],
+      width: jsonData['width'],
+      weight: jsonData['weight'],
+    );
+  }
+
+  static Map<String, dynamic> toMap(Barbell barbell) => {
+        'id': barbell.id,
+        'name': barbell.name,
+        'width': barbell.width,
+        'weight': barbell.weight,
+      };
+
+  static String encode(List<Barbell> musics) => json.encode(
+        musics
+            .map<Map<String, dynamic>>((music) => Barbell.toMap(music))
+            .toList(),
+      );
+
+  static List<Barbell> decode(String musics) =>
+      (json.decode(musics) as List<dynamic>)
+          .map<Barbell>((item) => Barbell.fromJson(item))
+          .toList();
+}
--- barbell_calc/lib/inventory.dart
+++ barbell_calc/lib/inventory.dart
@@ -1,7 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'barbell.dart';
 
 class InventoryPage extends StatelessWidget {
   const InventoryPage({super.key});
@@ -169,8 +168,6 @@ class _AddBarbellState extends State<AddBarbell> {
     final String? barbellsString = prefs.getString('barbells_key');
     final List<Barbell> barbells = Barbell.decode(barbellsString!);
 
-
-
     setState(() {
       // reformatting text inputs (remove spaces and minus, replace , with .)
       weightController.text = weightController.text
@@ -180,8 +177,6 @@ class _AddBarbellState extends State<AddBarbell> {
 
       print(barbells.first.name);
     });
-
-
   }
 
   @override
@@ -248,44 +243,4 @@ class _AddBarbellState extends State<AddBarbell> {
     weightController.dispose();
     super.dispose();
   }
-}
-
-class Barbell {
-  final int id;
-  final String name, width;
-  final double weight;
-
-  Barbell({
-    required this.id,
-    required this.name,
-    required this.width,
-    required this.weight,
-  });
-
-  factory Barbell.fromJson(Map<String, dynamic> jsonData) {
-    return Barbell(
-      id: jsonData['id'],
-      name: jsonData['name'],
-      width: jsonData['width'],
-      weight: jsonData['weight'],
-    );
-  }
-
-  static Map<String, dynamic> toMap(Barbell barbell) => {
-        'id': barbell.id,
-        'name': barbell.name,
-        'width': barbell.width,
-        'weight': barbell.weight,
-      };
-
-  static String encode(List<Barbell> musics) => json.encode(
-        musics
-            .map<Map<String, dynamic>>((music) => Barbell.toMap(music))
-            .toList(),
-      );
-
-  static List<Barbell> decode(String musics) =>
-      (json.decode(musics) as List<dynamic>)
-          .map<Barbell>((item) => Barbell.fromJson(item))
-          .toList();
 }
