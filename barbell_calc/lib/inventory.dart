688136389ce59521f0dfecb6140692689ff48529
Felix Peer <felixpeerpi@gmail.com>
2024-01-07 15:44:12 +0100

    added inventory scaffold

--- barbell_calc/lib/inventory.dart
+++ barbell_calc/lib/inventory.dart
@@ -5,6 +5,12 @@ class InventoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Inventory')),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Placeholder(),
+      ),
+    );
   }
 }
