e44899fa4f93e0b3e3973fd453294aba8660e513
Felix Peer <felixpeerpi@gmail.com>
2024-01-07 23:09:15 +0100

    removed unused imports

--- barbell_calc/lib/inventory.dart
+++ barbell_calc/lib/inventory.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'barbell.dart';
 import 'barbellinventory.dart';
 
 class InventoryPage extends StatelessWidget {
