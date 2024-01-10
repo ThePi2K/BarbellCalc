ee62b33c0a5fdba1432511315925a30e2508ab90
Felix Peer <felixpeerpi@gmail.com>
2024-01-10 23:33:15 +0100

    Test ExpansionPanelList

--- barbell_calc/lib/inventory.dart
+++ barbell_calc/lib/inventory.dart
@@ -712,17 +712,48 @@ class BarbellListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: barbellList.length,
-      itemBuilder: (BuildContext context, int index) {
-        return BarbellListItem(
-          barbell: barbellList[index],
-          index: index,
-          barbellListLength: barbellList.length,
-          onDelete: deleteBarbell,
-        );
-      },
+    return ExpansionPanelList(
+      expansionCallback: (int index, bool isExpanded) {},
+      children: [
+        ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return ListTile(
+              title: Text('Item 1'),
+            );
+          },
+          body: ListTile(
+            title: Text('Item 1 child'),
+            subtitle: Text('Details goes here'),
+          ),
+          isExpanded: true,
+        ),
+        ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return ListTile(
+              title: Text('Item 2'),
+            );
+          },
+          body: ListTile(
+            title: Text('Item 2 child'),
+            subtitle: Text('Details goes here'),
+          ),
+          isExpanded: false,
+        ),
+      ],
     );
+
+
+    // ListView.builder(
+      // itemCount: barbellList.length,
+      // itemBuilder: (BuildContext context, int index) {
+      //   return BarbellListItem(
+      //     barbell: barbellList[index],
+      //     index: index,
+      //     barbellListLength: barbellList.length,
+      //     onDelete: deleteBarbell,
+      //   );
+      // },
+    // );
   }
 }
 
