36172d822649e6daa95b63d7f5357661f5765380
Felix Peer <felixpeerpi@gmail.com>
2024-01-17 11:54:13 +0100

    temp

--- barbell_calc/lib/inventory.dart
+++ barbell_calc/lib/inventory.dart
@@ -275,7 +275,9 @@ class NewPlateBarbellButton extends StatelessWidget {
                         leading: SvgPicture.asset(
                           'assets/icons/plate.svg',
                           height: 30,
-                          theme: SvgTheme(currentColor: Theme.of(context).colorScheme.onSurface),
+                          theme: SvgTheme(
+                              currentColor:
+                                  Theme.of(context).colorScheme.onSurface),
                           // color: Theme.of(context).colorScheme.onSurface,
                         ),
                         title: const Text('Add Plate'),
@@ -836,18 +838,15 @@ class _CreateBarbellState extends State<CreateBarbell> {
 }
 
 class EditBarbell extends StatefulWidget {
-  const EditBarbell(
-      {super.key, required this.onSave, required this.barbells});
+  EditBarbell({super.key, required this.barbellToEdit});
 
-  final Function() onSave;
-  final List<Barbell> barbells;
+  Barbell barbellToEdit;
 
   @override
   State<EditBarbell> createState() => _EditBarbellState();
 }
 
 class _EditBarbellState extends State<EditBarbell> {
-
   final TextEditingController nameController = TextEditingController();
   final TextEditingController weightController = TextEditingController();
 
@@ -868,7 +867,7 @@ class _EditBarbellState extends State<EditBarbell> {
       if (widthList.isNotEmpty) {
         dropdownValue = widthList.first;
         setState(
-                () {}); // Trigger a rebuild to update the UI after dropdownValue is set
+            () {}); // Trigger a rebuild to update the UI after dropdownValue is set
       }
     });
   }
@@ -1027,7 +1026,7 @@ class _EditBarbellState extends State<EditBarbell> {
 
                   // check if plate is already saved
                   bool isBarbellDouble = widget.barbells.any((barbell) =>
-                  barbell.weight == barbellToAdd.weight &&
+                      barbell.weight == barbellToAdd.weight &&
                       barbell.width == barbellToAdd.width &&
                       barbell.name == barbellToAdd.name);
                   if (isBarbellDouble) {
@@ -1045,7 +1044,7 @@ class _EditBarbellState extends State<EditBarbell> {
                         builder: (BuildContext context) {
                           return ErrorDialog(
                               errorMessage:
-                              'The Barbell has to weight more than 0 ${metricSystem ? 'kg' : 'lb'}!');
+                                  'The Barbell has to weight more than 0 ${metricSystem ? 'kg' : 'lb'}!');
                         },
                       );
                     } else {
@@ -1070,7 +1069,6 @@ class _EditBarbellState extends State<EditBarbell> {
     );
   }
 }
-
 
 class ErrorDialog extends StatelessWidget {
   const ErrorDialog({super.key, required this.errorMessage});
@@ -1223,7 +1221,15 @@ class BarbellListItem extends StatelessWidget {
             IconButton(
               icon: const Icon(Icons.edit),
               onPressed: () {
-                // EDIT BARBELL
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return EditBarbell(
+                      onSave: onSaveBarbell,
+                      barbells: barbells,
+                    );
+                  },
+                );
               },
             ),
             IconButton(
@@ -1235,7 +1241,8 @@ class BarbellListItem extends StatelessWidget {
                     builder: (BuildContext context) {
                       return AlertDialog(
                         title: const Text('Attention'),
-                        content: Text('You need at least one ${barbell.width} Barbell!'),
+                        content: Text(
+                            'You need at least one ${barbell.width} Barbell!'),
                         actions: [
                           TextButton(
                             onPressed: () {
@@ -1253,7 +1260,8 @@ class BarbellListItem extends StatelessWidget {
                     builder: (BuildContext context) {
                       return AlertDialog(
                         title: const Text('Attention'),
-                        content: const Text('Do really want to remove this Barbell?'),
+                        content: const Text(
+                            'Do really want to remove this Barbell?'),
                         actions: [
                           TextButton(
                             onPressed: () {
